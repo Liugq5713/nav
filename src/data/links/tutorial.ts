d027124c9d909ebfe1e1ba3df15d59bb39938021
liugq <liugq5713@gmail.com>
2019-06-10 13:00:34 +0800

    d

--- src/data/links/tutorial.ts
+++ src/data/links/tutorial.ts
@@ -14,5 +14,16 @@ export const TutorialLinks = [
   {
     label: 'Linux',
     value: 'https://www.digitalocean.com/community'
+  },
+  {
+    label:'NodeJS技术栈',
+    value:'https://www.nodejs.red/#/README'
+  },{
+    label:"Vue技术揭秘",
+    value:'https://ustbhuangyi.github.io/vue-analysis/'
+  },
+  {
+    label:"Vue技术内幕",
+    value:'http://hcysun.me/vue-design/art/'
   }
 ]
