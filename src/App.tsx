c8f469d909048eedbf5d149721946103a479801f
liugq <liugq5713@gmail.com>
2019-06-12 14:48:16 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css'
 import ArrayLink from '../src/components/ArrayLink'
-import ObjectLink from '../src/components/ObjectLink'
 import GithubCorner from '../src/components/GithubCorner'
 import Search from '../src/components/Search'
 import { ReadLinks } from './data/links/read'
@@ -14,7 +13,12 @@ import {
   GithubLinks
 } from './data/links/book'
 // import { SelfLinks } from './data/links/self'
-import { ToolLinks } from './data/links/tool'
+import {
+  CodeLinks,
+  FileLinks,
+  ImageLinks,
+  SourceLinks
+} from './data/links/tool'
 const App: React.FC = () => {
   const options = ReadLinks.concat(
     TutorialLinks,
@@ -34,8 +38,10 @@ const App: React.FC = () => {
           <ArrayLink title='教程' links={TutorialLinks} />
         </div>
         <div className='pure-u-1  pure-u-sm-1-2 pure-u-md-1-3 pure-u-xl-1-3'>
-          <h1>工具</h1>
-          <ObjectLink links={ToolLinks} />
+          <ArrayLink title='代码' links={CodeLinks} />
+          <ArrayLink title='文件处理' links={FileLinks} />
+          <ArrayLink title='图片处理' links={ImageLinks} />
+          <ArrayLink title='资源' links={SourceLinks} />
         </div>
         <div className='pure-u-1  pure-u-sm-1-2 pure-u-md-1-3 pure-u-xl-1-3'>
           <ArrayLink title='收集' links={BookLinks} />
--- src/data/links/tool.ts
+++ src/data/links/tool.ts
@@ -1,130 +1,98 @@
-interface link {
-  code?: {
-    label: string
-    children: Object
-  }
-  tool?: {
-    label: string
-    children: Object
-  }
-  pic?: {
-    label: string
-    children: Object
-  }
-  resource?: {
-    label: string
-    children: Object
-  }
-  [propName: string]: any
-}
-export const ToolLinks: link = {
-  code: {
-    label: '代码',
-    children: [
-      {
-        label: 'JSON 美化',
-        value: 'https://www.json.cn/'
-      },
-      {
-        label: '练习打字',
-        value: 'https://www.typingclub.com'
-      },
-      {
-        label: '微信公众号排版',
-        value: 'https://lab.lyric.im/wxformat/'
-      },
-      {
-        label: 'jsBin',
-        value: 'http://jsbin.com/'
-      },
-      {
-        label: 'jsFiddle',
-        value: 'https://jsfiddle.net/'
-      },
-      {
-        label: '测试 JS 性能',
-        value: 'https://www.measurethat.net/Benchmarks'
-      },
-      {
-        label: '算法可视化',
-        value: 'https://visualgo.net/en'
-      }
-    ]
+export const CodeLinks = [
+  {
+    label: 'JSON 美化',
+    value: 'https://www.json.cn/'
   },
-  tool: {
-    label: '文件处理',
-    children: [
-      {
-        label: 'PDF 处理',
-        value: 'https://www.ilovepdf.com/'
-      }
-    ]
+  {
+    label: '练习打字',
+    value: 'https://www.typingclub.com'
   },
+  {
+    label: '微信公众号排版',
+    value: 'https://lab.lyric.im/wxformat/'
+  },
+  {
+    label: 'jsBin',
+    value: 'http://jsbin.com/'
+  },
+  {
+    label: 'jsFiddle',
+    value: 'https://jsfiddle.net/'
+  },
+  {
+    label: '测试 JS 性能',
+    value: 'https://www.measurethat.net/Benchmarks'
+  },
+  {
+    label: '算法可视化',
+    value: 'https://visualgo.net/en'
+  }
+]
 
-  pic: {
-    label: '图片处理',
-    children: [
-      {
-        label: '生成代码图片',
-        value: 'https://carbon.now.sh/'
-      },
-      {
-        label: '阿里巴巴矢量图标库',
-        value: 'http://www.iconfont.cn/plus/search/index'
-      },
-      {
-        label: '比特虫',
-        value: 'http://www.bitbug.net/'
-      },
-      {
-        label: 'Favicon Generator',
-        value: 'https://realfavicongenerator.net/'
-      },
-      {
-        label: '调整图片大小',
-        value: 'https://www.befunky.com/create/resize-image/'
-      },
-      {
-        label: 'tinypng',
-        value: 'https://tinypng.com/'
-      },
-      {
-        label: 'ImageMagick',
-        value: 'https://www.imagemagick.org/script/command-line-processing.php'
-      },
-      {
-        label: 'pngquant',
-        value: 'https://github.com/kornelski/pngquant'
-      },
-      {
-        label: 'pngquant GUI 工具',
-        value: 'https://pngquant.org/PNGoo.0.1.1.zip'
-      },
-      {
-        label: 'ASCII 码字符画在线绘制',
-        value: 'http://asciiflow.com/'
-      }
-    ]
-  },
-  resource: {
-    label: '资源',
-    children: [
-      {
-        label: '渐变色网站',
-        value: 'https://uigradients.com/#Wiretap'
-      },
-      {
-        label: '非常好看的图片',
-        value: 'https://dribbble.com/'
-      },
-      {
-        label: '免费高清图片',
-        value: 'https://pixabay.com/zh/photos/'
-      },
-      {
-        label: 'unDraw矢量图',
-        value: 'https://undraw.co/illustrations'
-      }
-    ]
+export const FileLinks = [
+  {
+    label: 'PDF 处理',
+    value: 'https://www.ilovepdf.com/'
   }
-}
+]
+export const ImageLinks = [
+  {
+    label: '生成代码图片',
+    value: 'https://carbon.now.sh/'
+  },
+  {
+    label: '阿里巴巴矢量图标库',
+    value: 'http://www.iconfont.cn/plus/search/index'
+  },
+  {
+    label: '比特虫',
+    value: 'http://www.bitbug.net/'
+  },
+  {
+    label: 'Favicon Generator',
+    value: 'https://realfavicongenerator.net/'
+  },
+  {
+    label: '调整图片大小',
+    value: 'https://www.befunky.com/create/resize-image/'
+  },
+  {
+    label: 'tinypng',
+    value: 'https://tinypng.com/'
+  },
+  {
+    label: 'ImageMagick',
+    value: 'https://www.imagemagick.org/script/command-line-processing.php'
+  },
+  {
+    label: 'pngquant',
+    value: 'https://github.com/kornelski/pngquant'
+  },
+  {
+    label: 'pngquant GUI 工具',
+    value: 'https://pngquant.org/PNGoo.0.1.1.zip'
+  },
+  {
+    label: 'ASCII 码字符画在线绘制',
+    value: 'http://asciiflow.com/'
+  }
+]
+
+export const SourceLinks = [
+  {
+    label: '渐变色网站',
+    value: 'https://uigradients.com/#Wiretap'
+  },
+  {
+    label: '非常好看的图片',
+    value: 'https://dribbble.com/'
+  },
+  {
+    label: '免费高清图片',
+    value: 'https://pixabay.com/zh/photos/'
+  },
+  {
+    label: 'unDraw矢量图',
+    value: 'https://undraw.co/illustrations'
+  }
+]
