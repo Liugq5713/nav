0a15055b418dfead2c2082f2560d0a7bac1c026b
liugq <liugq5713@gmail.com>
2019-06-26 17:51:22 +0800

    feat: add logo

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react'
+import dinodr from './dinodr.gif'
 import './style.css'
 interface SearchFunc {
   (source: string): void
@@ -13,21 +14,18 @@ const SideBar: React.FC<PropType> = props => {
   const { cateogries } = props
   const cateogry_list = cateogries.map(item => {
     return (
-      <li
-        className='pure-menu-item'
-        value={item}
-        key={item}
-      >
-        <a href={`#${item}`}  className="pure-menu-link ">
-          <span className='color-white'>
-              {item}
-          </span>
+      <li className='pure-menu-item' value={item} key={item}>
+        <a href={`#${item}`} className='pure-menu-link '>
+          <span className='color-white'>{item}</span>
         </a>
       </li>
     )
   })
   return (
     <div className='sidebar'>
+      <div className='logo'>
+        <img width='100%' src={dinodr} alt='logo' />
+      </div>
       <div className='header'>{props.children}</div>
       <div className='content'>
         <ul className='pure-menu-list'>{cateogry_list}</ul>
