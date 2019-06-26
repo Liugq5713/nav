4b7e89af757a4f02c7fdd4a35a6dfc52fa3b8d2d
liugq <liugq5713@gmail.com>
2019-06-26 18:04:53 +0800

    xxx

---
+++ src/components/DropDown/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DropDown: React.FC = props => {
+  return (
+    <div>
+      <h3>houhou</h3>
+      <div>{props.children}</div>
+    </div>
+  )
+}
+
+export default DropDown
