ce732ee27059900216ea4cb2fbbb9aa715ab1e6d
liugq <liugq5713@gmail.com>
2019-07-26 13:26:38 +0800

    fix: ci

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react'
-import dinodr from './dinodr.gif'
+// import dinodr from './dinodr.gif'
 import './style.css'
 interface SearchFunc {
   (source: string): void
