4cd4f6a186caabb772814a46c75d382e4176fb9a
liugq <liugq5713@gmail.com>
2019-08-30 16:26:40 +0800

    test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,14 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import "./style.css"
 const Header: React.FC = () => {
+  const [weather, setWeather] = useState()
+
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.type = "text/javascript"
+    script.src =
+      "https://api.seniverse.com/v3/weather/now.json?location=beijing&ts=1443079775&ttl=300&uid=[your_uid]&sig=[your_signature]&callback=showWeather"
+  })
   return (
     <header className="header">
       <section>我来组成头部</section>
